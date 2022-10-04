16e200f9cd83f3aaf54fd22e6b35b611ac7a18d1
fukuryu1902 <c0b211357f@edu.teu.ac.jp>
2022-10-04 16:25:16 +0900

    基本機能の拡張

--- ex02/calc.py
+++ ex02/calc.py
@@ -9,21 +9,24 @@ def button_click(event):
 
 def eq_button_click(event):
     get_num = entry.get()
-    result = eval(get_num)
-    entry.delete(0, tk.END)
-    entry.insert(tk.END, result)
+    try:
+        result = eval(get_num)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, result)
+    except SyntaxError:
+        entry.delete(0, tk.END)
 
 
 root = tk.Tk()
 root.title("電卓")
-root.geometry("300x600")
+root.geometry("600x650")
 
-entry = tk.Entry(justify="right", width=10, font=("Times New Roman", 40))
+entry = tk.Entry(justify="right", width=10,  font=("Times New Roman", 40))
 entry.grid(row=0, column=0, columnspan=3)
 
 r, c = 1, 0
-for i in range(9, -3, -1):
-    if i >= 0:
+for i in range(9, -6, -1):
+    if i > 0:
         button = tk.Button(root, text=str(i),width=4, height=2, font=("Times New Roman", 30))
         button.bind("<1>", button_click)
         button.grid(row=r, column=c)
@@ -31,10 +34,18 @@ for i in range(9, -3, -1):
         if c % 3 == 0:
             c = 0
             r += 1
+    elif i == 0:
+        button = tk.Button(root, text="0",width=4, height=2, font=("Times New Roman", 30))
+        button.bind("<1>", button_click)
+        button.grid(row=r, column=c+1)
+        c += 1
+        if c % 3 == 0:
+            c = 0
+            r += 1
     elif i == -1:
         button = tk.Button(root, text="+", width=4, height=2, font=("Times New Roman", 30))
         button.bind("<1>", button_click)
-        button.grid(row=r, column=c)
+        button.grid(row=r-2, column=c+2)
         c += 1
         if c % 3 == 0:
             c = 0
@@ -42,8 +53,31 @@ for i in range(9, -3, -1):
     elif i == -2:
         button = tk.Button(root, text="=", width=4, height=2, font=("Times New Roman", 30))
         button.bind("<1>", eq_button_click)
-        button.grid(row=r, column=c)
-    
+        button.grid(row=r, column=c+1)
+    elif i == -3:
+        button = tk.Button(root, text="-", width=4, height=2, font=("Times New Roman", 30))
+        button.bind("<1>", button_click)
+        button.grid(row=r-1, column=c+1)
+        c += 1
+        if c % 3 == 0:
+            c = 0
+            r += 1
+    elif i == -4:
+        button = tk.Button(root, text="*", width=4, height=2, font=("Times New Roman", 30))
+        button.bind("<1>", button_click)
+        button.grid(row=r-4, column=c+3)
+        c += 1
+        if c % 3 == 0:
+            c = 0
+            r += 1
+    elif i == -5:
+        button = tk.Button(root, text="/", width=4, height=2, font=("Times New Roman", 30))
+        button.bind("<1>", button_click)
+        button.grid(row=r-5, column=c+2)
+        c += 1
+        if c % 3 == 0:
+            c = 0
+            r += 1
     
 
 root.mainloop()
