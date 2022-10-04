90daf0af3c0e814acb513b92787ca3ff0179b2be
fukuryu1902 <c0b211357f@edu.teu.ac.jp>
2022-10-04 14:56:35 +0900

    練習問題2

--- ex02/calc.py
+++ ex02/calc.py
@@ -8,9 +8,12 @@ def button_click(event):
 
 root = tk.Tk()
 root.title("電卓")
-root.geometry("300x500")
+root.geometry("300x600")
 
-r, c = 0, 0
+entry = tk.Entry(justify="right", width=10, font=("Times New Roman", 40))
+entry.grid(row=0, column=0, columnspan=3)
+
+r, c = 1, 0
 for i in range(9, -1, -1):
     button = tk.Button(root, text=str(i),width=4, height=2, font=("Times New Roman", 30))
     button.bind("<1>", button_click)
@@ -19,5 +22,6 @@ for i in range(9, -1, -1):
     if c % 3 == 0:
         c = 0
         r += 1
+
 
 root.mainloop()
