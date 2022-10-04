5d16a5eb685469f1c504deb30e7aac507e096b7f
fukuryu1902 <c0b211357f@edu.teu.ac.jp>
2022-10-04 15:13:03 +0900

    練習問題6

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,7 +4,7 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
-    entry.insert(0, txt)
+    entry.insert(tk.END, txt)
     tkm.showinfo(txt, f"[{txt}]のボタンがクリックされました")
 
 root = tk.Tk()
@@ -15,14 +15,27 @@ entry = tk.Entry(justify="right", width=10, font=("Times New Roman", 40))
 entry.grid(row=0, column=0, columnspan=3)
 
 r, c = 1, 0
-for i in range(9, -1, -1):
-    button = tk.Button(root, text=str(i),width=4, height=2, font=("Times New Roman", 30))
-    button.bind("<1>", button_click)
-    button.grid(row=r, column=c)
-    c += 1
-    if c % 3 == 0:
-        c = 0
-        r += 1
-
+for i in range(9, -3, -1):
+    if i >= 0:
+        button = tk.Button(root, text=str(i),width=4, height=2, font=("Times New Roman", 30))
+        button.bind("<1>", button_click)
+        button.grid(row=r, column=c)
+        c += 1
+        if c % 3 == 0:
+            c = 0
+            r += 1
+    elif i == -1:
+        button = tk.Button(root, text="+", width=4, height=2, font=("Times New Roman", 30))
+        button.bind("<1>", button_click)
+        button.grid(row=r, column=c)
+        c += 1
+        if c % 3 == 0:
+            c = 0
+            r += 1
+    elif i == -2:
+        button = tk.Button(root, text="=", width=4, height=2, font=("Times New Roman", 30))
+        button.bind("<1>", button_click)
+        button.grid(row=r, column=c)
+    
 
 root.mainloop()
